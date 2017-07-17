3e631202ba06d9950949115aa7713ae327af2116
Lewis Freiberg <lewi@2pk.in>
2017-07-18 01:45:11 +1000

    Clean up index

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import Link from "next/link";
+
+import Layout from "../components/layout";
 
 const Title = styled.h1`
   color: rebeccapurple;
   font-size: 50px;
 `;
 
-export default () => (
-  <div>
-    <Title>My page</Title>
-    <div>Click <Link href="/about"><a>here</a></Link> to read more</div>
-  </div>
-);
+export default () =>
+  <Layout>
+    <div>Click to read more</div>
+  </Layout>;
