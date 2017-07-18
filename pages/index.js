9e994e042673a88cad2a593ff67c79f4126b40cc
Lewis Freiberg <lewi@2pk.in>
2017-07-19 00:42:06 +1000

    Create a dummy page

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,37 @@ import React from "react";
 import styled from "styled-components";
 
 import Layout from "../components/layout";
-
+import { Row, Col } from "../components/scaffold";
 import Satoshi from "../components/satoshi";
 
 export default () =>
   <Layout>
     <h2>Home Page</h2>
-    <Satoshi />
+    <Row justify={"space-between"}>
+      <Satoshi price={5021000} height={200} />
+    </Row>
+    <Row justify={"space-around"}>
+      <Satoshi price={42000} />
+      <Satoshi price={100000000000000} />
+    </Row>
+    <Row justify={"space-between"}>
+      <Satoshi
+        type={`image`}
+        image={`http://goods.satoshipay.de/placeholder/2.png`}
+        price={42000}
+      />
+      <Satoshi
+        type={`image`}
+        image={`http://goods.satoshipay.de/placeholder/1.png`}
+        price={100000000000000}
+      />
+    </Row>
+    <Row justify={"space-between"}>
+      <Satoshi
+        type={`video`}
+        height={400}
+        image={`http://goods.satoshipay.de/placeholder/The-nature-of-Montenegro-SD.png`}
+        price={42000}
+      />
+    </Row>
   </Layout>;
