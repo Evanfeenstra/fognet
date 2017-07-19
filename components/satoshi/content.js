e1f173ee396448b4599dec8593c29dc76ddedc25
Lewis Freiberg <lewi@2pk.in>
2017-07-20 00:18:34 +1000

    Create content display modules

---
+++ components/satoshi/content.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export default props => {
+  if (props.type === "image") {
+    return (
+      <Content {...props}>
+        {props.content && <Image {...props} src={props.content} />}
+      </Content>
+    );
+  } else if (props.type === "video") {
+    return <Video {...props} src={props.content} autoPlay controls />;
+  } else {
+    return (
+      <Content {...props} dangerouslySetInnerHTML={{ __html: props.content }} />
+    );
+  }
+};
+
+const Content = styled.div`
+  position: relative;
+  min-width: 200px;
+  width: auto;
+  min-height: 80px;
+  height: auto;
+`;
+
+const Image = styled.img`
+  height: ${props => (props.height ? props.height + `px` : "auto")};
+  max-width: 100%;
+`;
+
+const Video = styled.video`
+  position: relative;
+  min-width: 200px;
+  width: 100%;
+  min-height: 80px;
+  height: auto;
+`;
