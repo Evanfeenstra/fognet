08c9fee44257d2002a3bee380b8b90e9619154bb
Lewis Freiberg <lewi@2pk.in>
2017-07-20 00:27:19 +1000

    Dumb data

--- pages/index.js
+++ pages/index.js
@@ -7,31 +7,45 @@ import Satoshi from "../components/satoshi";
 
 export default () =>
   <Layout>
-    <h2>Home Page</h2>
     <Row justify={"space-between"}>
-      <Satoshi price={5021000} height={200} />
+      <Satoshi
+        height={130}
+        price={5021000}
+        content={`<p>Picanha tri-tip shank, chicken ham hock kevin shoulder ham. Shankle rump landjaeger corned beef jerky bacon boudin tri-tip. Boudin chicken short ribs ham salami kielbasa shank biltong frankfurter ball tip. Venison meatloaf burgdoggen, cow filet mignon meatball capicola spare ribs beef ribs ribeye corned beef. Venison pastrami tail landjaeger ball tip doner.</p>`}
+      />
     </Row>
     <Row justify={"space-around"}>
-      <Satoshi price={42000} />
-      <Satoshi price={100000000000000} />
+      <Satoshi
+        height={100}
+        price={42000}
+        content={`<h2>Hello</h2><br/><p>Picanha tri-tip shank, chicken ham hock kevin shoulder ham. Shankle rump landjaeger corned beef jerky bacon boudin tri-tip. Boudin chicken short ribs ham salami kielbasa shank biltong frankfurter ball tip. Venison meatloaf burgdoggen, cow filet mignon meatball capicola spare ribs beef ribs ribeye corned beef. Venison pastrami tail landjaeger ball tip doner.</p>`}
+      />
+      <Satoshi
+        height={100}
+        content={`<h2>Hello</h2><br/><p>Picanha tri-tip shank, chicken ham hock kevin shoulder ham. Shankle rump landjaeger corned beef jerky bacon boudin tri-tip. Boudin chicken short ribs ham salami kielbasa shank biltong frankfurter ball tip. Venison meatloaf burgdoggen, cow filet mignon meatball capicola spare ribs beef ribs ribeye corned beef. Venison pastrami tail landjaeger ball tip doner.</p>`}
+        price={100000000000000}
+      />
     </Row>
     <Row justify={"space-between"}>
       <Satoshi
         type={`image`}
-        image={`http://goods.satoshipay.de/placeholder/2.png`}
+        placeholder={`/static/potatoplace.png`}
+        content={`/static/potato.png`}
         price={42000}
       />
       <Satoshi
         type={`image`}
-        image={`http://goods.satoshipay.de/placeholder/1.png`}
+        placeholder={`http://goods.satoshipay.de/placeholder/1.png`}
+        content={`/static/image1.png`}
         price={100000000000000}
       />
     </Row>
     <Row justify={"space-between"}>
       <Satoshi
         type={`video`}
-        height={400}
-        image={`http://goods.satoshipay.de/placeholder/The-nature-of-Montenegro-SD.png`}
+        height={480}
+        placeholder={`https://cheproximity.com.au/assets/Capabilities/capabilities-mobile2.jpg`}
+        content={`https://cheproximity.com.au/assets/Capabilities/CHEP-capabilities-main.webm`}
         price={42000}
       />
     </Row>
