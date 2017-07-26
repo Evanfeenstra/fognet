5f09a97873948ecafb4ba7f94b91ecd9ebbb9317
Lewis Freiberg <lewi@2pk.in>
2017-07-26 17:42:43 +1000

    Update IOTA libs

--- libs/iota.js
+++ libs/iota.js
@@ -5,7 +5,7 @@ var iota = new IOTA({
   provider: "https://node.tangle.works"
 });
 
-export default class iotaLib {
+export default class Iota {
   static info = () => {
     iota.api.getNodeInfo(function(error, success) {
       if (error) {
@@ -15,4 +15,96 @@ export default class iotaLib {
       }
     });
   };
+
+  static initialise = () => {
+    // Escape the function when server rendering
+    if (!isWindow()) return;
+    // Grab seed from local
+    var user = get("user");
+    // No seed? Then go make one!
+    if (user === null) {
+      console.log("No User");
+      user = Iota.setupUser();
+    }
+    // Now get an address
+  };
+
+  static setupUser = async () => {
+    var user = { seed: seedGen(81), index: 1, addresses: [], purchases: [] };
+    user.push(await getAddress(user.seed, user.index));
+    set("user", user);
+    console.log(user);
+    return user;
+  };
+
+  static getBalance = () => {
+    iota.api.getNodeInfo(function(error, success) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(success);
+      }
+    });
+  };
+  static purchaseItem = () => {
+    iota.api.getNodeInfo(function(error, success) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(success);
+      }
+    });
+  };
 }
+
+////// HELPERS
+
+// Get a new Address
+const getAddress = async (seed, index) => {
+  var digest = iota.multisig.getDigest(seed, index, 2);
+  console.log(digest);
+  // try {
+  //   let response = await fetch("", { body: { digest, index } });
+  //   let responseJson = await response.json();
+  //   return responseJson.address;
+  // } catch (error) {
+  //   console.error(error);
+  // }
+};
+
+// Generate a random seed. Higher security needed
+const seedGen = length => {
+  var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ9";
+  var i;
+  var result = "";
+  if (window.crypto && window.crypto.getRandomValues) {
+    var values = new Uint32Array(length);
+    window.crypto.getRandomValues(values);
+    for (i = 0; i < length; i++) {
+      result += charset[values[i] % charset.length];
+    }
+    return result;
+  } else
+    throw new Error(
+      "Your browser sucks and can't generate secure random numbers"
+    );
+};
+
+// GET from localStorage
+const get = item => {
+  return JSON.parse(localStorage.getItem(item));
+};
+
+// SET item to localStorage
+const set = (item, data) => {
+  localStorage.setItem(item, JSON.stringify(data));
+};
+
+// Check if window is available
+const isWindow = () => {
+  if (typeof window === "undefined" || typeof localStorage === "undefined") {
+    global.localStorage = {};
+    return false;
+  }
+  return true;
+};
