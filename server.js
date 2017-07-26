737ab8e3fdac51297646128b95840fdee737c431
Lewis Freiberg <lewi@2pk.in>
2017-07-26 17:53:38 +1000

    Add KOA for Chris

---
+++ server.js
@@ -0,0 +1,30 @@
+const Koa = require("koa");
+const next = require("next");
+const Router = require("koa-router");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = new Koa();
+  const router = new Router();
+
+  /// Add stuff here ^^
+
+  router.get("*", async ctx => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+
+  server.use(async (ctx, next) => {
+    ctx.res.statusCode = 200;
+    await next();
+  });
+
+  server.use(router.routes());
+  server.listen(3000, err => {
+    if (err) throw err;
+    console.log("> Ready on http://localhost:3000");
+  });
+});
