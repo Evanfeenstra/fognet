998c19054d760c5a2b82babd2ea381cfd0311592
Lewis Freiberg <lewi@2pk.in>
2017-08-17 12:09:34 +1000

    Update the paywall placeholder

--- components/satoshi/paywall.js
+++ components/satoshi/paywall.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export default props => {
   if (props.type === "image") {
@@ -7,25 +7,25 @@ export default props => {
         {props.placeholder && <Image src={props.placeholder} />}
         {props.children}
       </Paywall>
-    );
+    )
   } else if (props.type === "video") {
     return (
       <Video {...props}>
-        <img
+        {/* <img
           style={{ width: 100, height: 100, opacity: 0.3 }}
           src={`/static/play.png`}
-        />
+        /> */}
         {props.children}
       </Video>
-    );
+    )
   } else {
     return (
       <Generic {...props}>
         {props.children}
       </Generic>
-    );
+    )
   }
-};
+}
 
 const Paywall = styled.div`
   margin-bottom: ${props => (props.margin ? "2rem" : "none")};
@@ -33,12 +33,12 @@ const Paywall = styled.div`
   height: ${props => (props.height ? props.height + `px` : "auto")};
   min-width: 200px;
   min-height: 80px;
-`;
+`
 
 const Image = styled.img`
   height: 100%;
   max-width: 100%;
-`;
+`
 
 const Video = Paywall.extend`
   display: flex;
@@ -47,9 +47,9 @@ const Video = Paywall.extend`
   align-items: center;
   background: url(${props => props.placeholder}) no-repeat center center;
   background-size: cover;
-`;
+`
 const Generic = Paywall.extend`
   background: linear-gradient(#ffeacc 90%, white 0);
   background-size: 100% 20px;
   flex: ${props => (props.flex ? props.flex : 1)};
-`;
+`
