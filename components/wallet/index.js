b7642a62251e38f9895d892f40e576f85e72b55d
Lewis Freiberg <lewi@2pk.in>
2017-08-18 16:32:42 +1000

    Initialise channel on load

--- components/wallet/index.js
+++ components/wallet/index.js
@@ -1,25 +1,28 @@
-import React from "react";
-import styled from "styled-components";
-import Fab from "./fab";
-import Wallet from "./wallet";
+import React from "react"
+import styled from "styled-components"
+import Fab from "./fab"
+import Wallet from "./wallet"
+import Channel, { isWindow } from "../../libs/channel"
 
 export default class extends React.Component {
   state = {
     balance: 1000000000,
     drawerOpen: false
-  };
+  }
 
   toggle = () => {
-    this.setState({ drawerOpen: !this.state.drawerOpen });
-  };
+    this.setState({ drawerOpen: !this.state.drawerOpen })
+  }
 
   render() {
-    var { drawerOpen, balance } = this.state;
+    isWindow()
+    Channel.initialize()
+    var { drawerOpen, balance } = this.state
     return (
       <div>
         <Wallet {...this.state} toggle={this.toggle} />
         <Fab balance={balance} toggle={this.toggle} />
       </div>
-    );
+    )
   }
 }
