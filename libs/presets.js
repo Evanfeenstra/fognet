2849dd5f9b5cff06ef356950d04843c5d24b99aa
Lewis Freiberg <lewi@2pk.in>
2017-08-23 21:57:11 +1000

    Weird checsum error

--- libs/presets.js
+++ libs/presets.js
@@ -4,7 +4,7 @@ export default  {
     API: prod
       ? 'https://iotapay-server.now.sh/'
       : 'http://localhost:9000/',
-      ADDRESS: `GQMHDLS9XPSNURUCPKKJJTULZRPH9WSKUKQQQPJOY9CPRCNAUSIFWCLHVDSUHJJCPMQDARUIFFXKXFVQDBCXTTSCTD`,
+      ADDRESS: `GQMHDLS9XPSNURUCPKKJJTULZRPH9WSKUKQQQPJOY9CPRCNAUSIFWCLHVDSUHJJCPMQDARUIFFXKXFVQD`,
       IOTA: prod
       ? 'https://node.tangle.works'
       : 'http://52.58.212.188:14700',
